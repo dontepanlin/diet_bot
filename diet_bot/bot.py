976e8e3a9800bf58538334aa8fab6f79807ad4b6
unclehook <unclehook@ispras.ru>
2025-03-02 23:37:53 +0300

    speech kit

--- diet_bot/bot.py
+++ diet_bot/bot.py
@@ -11,18 +11,21 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiofile import async_open
 from aiohttp import ClientSession
 from pydantic import BaseModel, Field
 from pydantic_core import from_json
 
 TOKEN = getenv("BOT_TOKEN")
 YA_TOKEN = getenv("YA_TOKEN")
+YA_DIR = getenv("YA_DIR")
 
-if TOKEN is None or YA_TOKEN is None:
+if TOKEN is None or YA_TOKEN is None or YA_DIR is None:
     sys.exit(-1)
 
 IAM_TOKENS_URL = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
 LAST_TOKEN_NAME = ".token"
+SPEECH_V1_URL = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?topic=general&folderId={YA_DIR}&lang=ru-RU"
 
 
 class IamToken(BaseModel):
@@ -52,6 +55,9 @@ class IamToken(BaseModel):
         if not self.valid():
             await self.update()
 
+    def bearer_header(self) -> str:
+        return f"Bearer {self.token}"
+
 
 IAM_TOKEN = IamToken()
 if path.exists(LAST_TOKEN_NAME):
@@ -76,7 +82,18 @@ async def voice_handler(message: Message) -> None:
     logging.info(filename)
     await bot.download(message.voice, filename)
     await IAM_TOKEN.valid_or_update()
-    await message.answer(str(message.voice.file_size))
+
+    async with async_open(filename, 'rb') as afp:
+        async with ClientSession() as session:
+            print(IAM_TOKEN.bearer_header())
+            async with session.post(
+                url=SPEECH_V1_URL,
+                data=await afp.read(),
+                headers={"Authorization": IAM_TOKEN.bearer_header()}
+            ) as resp:
+                result = await resp.json()
+    print(result)
+    await message.answer(result["result"])
 
 
 async def main() -> None:
